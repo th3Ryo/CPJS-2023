f7ae1867d7a0f4b3092b3a768f7ecafb6b808d57
ryo <fabian_b.g@hotmail.com>
2023-04-09 20:39:42 -0500

    segundo commit

---
+++ scrit.js
@@ -0,0 +1,23 @@
+//css h1 { color:red }
+/*
+.parrafito {....}
+#pid {....}   */
+
+const h1 = document.querySelector(`h1`);
+const p = document.querySelector(`p`);
+const parrafo = document.querySelector(".parrafo");
+const pid = document.querySelector("#pid");
+const input = document.querySelector(`input`);
+
+console.log(input)
+console.log(input.value)
+
+
+console.log({
+    h1,
+    p,
+    parrafo,
+    pid,
+    input,
+}
+);
