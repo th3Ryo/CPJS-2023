6480d52936649136c56977fba0a659c2116b9f70
ryo <fabian_b.g@hotmail.com>
2023-04-17 16:55:36 -0500

    apuntes git

--- scrit.js
+++ scrit.js
@@ -17,3 +17,44 @@ form.addEventListener(`submit`,sumarValores);
     resultado.innerText = "Resultado: " + sumardatos;
  };
  //atajo para comentar
+/* 
+apuntes gifhub juan david
+
+github 
+
+new repository
+
+nombre del repositorio creado
+
+descripcion del repositorio
+
+en clase no creana nada de archivos desde la pagina
+
+1 git ini -- inicializa el git
+
+ls -lf -- muestra todas las carpetas incluidas las acultas
+
+crear de forma manual el gitignore -- para las carpetas que desea ignorar ej .vscode
+se recomierda visitar gitignore.io hay se debe aplicar tags
+ejemplo
+winwows, mac linix vscode node etc y se le da crear
+
+2 git add .gitignore
+
+3 git status -- es para  verificar que carpetas esta tendo del gib
+
+para este caso se va a la seccion de vscode 
+el agrega una linea con "".vscode" para que no suba nada de archivos de git
+ 
+4 git add -A
+
+5 git commit -m "primer texto"
+
+6 git remote -- verificar que no esta enlazado a ningun link
+
+7 git branch -M main
+
+8 git remote ..... el link sale del gif
+
+9 git push -u origin main
+  */
